f57db92d5909c15199bc8a8bc3d6fb8ab51e6e83
Hegly <clasesccnn1@gmail.com>
2024-02-16 00:38:15 -0600

    Styles y cambios

--- js/index.js
+++ js/index.js
@@ -3,15 +3,24 @@ fetch("https://fakestoreapi.com/products")
     .then(data => {
         let card = '';
         for (let db of data) {
-            card += `<div class="card mb-5" style="width: 18rem;">
-                        <img src="${db.image}" class="card-img-top" alt="${db.title}">
+            card += `<div class="card border border-success border-2 mb-3 shadow p-3 mb-5 bg-body rounded" style="max-width: 700px;">
+                    <div class="row g-0">
+                    <div class="col-4">
+                        <img src="${db.image}" class="card-img-top mt-3 mb-3" alt="${db.title}">
+                    </div>
+                    <div class="col-8">
                         <div class="card-body">
-                            <h3 class="card-title">${db.title}</h3>
-                            <h5 class="card-title">${db.price}</h5>
-                            <h6 class="card-title">${db.category}</h6>
-                            <p class="card-text">${db.description}</p>
+                        
+                            <h4 class="card-title text-center">${db.title}</h4>
                         </div>
-                     </div>`
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Precio:</strong> ${db.price}</li>
+                            <li class="list-group-item"><strong>Categoría:</strong> ${db.category}</li>
+                            <li class="list-group-item"><strong>Descripción:</strong> ${db.description}</li>
+                        </ul>
+                    </div>
+                    </div>
+                    </div>`
         }
         document.getElementById("lista").innerHTML = card
     })
